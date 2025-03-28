77d765fe9bcd20d3db2987f91936ec30263b9155
Zunayed <mdzunaed4@gmail.com>
2025-03-29 02:32:11 +0600

    urls

---
+++ lib/common/constants/urls.dart
@@ -0,0 +1,4 @@
+class Urls {
+  static const products = "https://fakestoreapi.com/products";
+  static const districts = "https://pencilbox.edu.bd/districts";
+}
--- lib/repository/item_repo.dart
+++ lib/repository/item_repo.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:dummy_app/common/constants/urls.dart';
 import 'package:dummy_app/models/product_model.dart';
 
 class ItemRepository {
   Dio dio = Dio();
   Future<List<Product>> getItems() async {
     List<Product> products = [];
-    final Response response = await dio.get("https://fakestoreapi.com/products");
+    final Response response = await dio.get(Urls.products);
     print("Status Code ======> ${response.statusCode}");
     print("Items Data ======> ${response.data}");
     if (response.statusCode == 200) {
